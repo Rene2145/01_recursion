865df5dba1beb6a7913cf838035804f373acc5fe
Rene David <tdarkreingreso@gmail.com>
2026-09-10 16:47:54 -0500

    Potencia n de un numero x

---
+++ Ejercicio_01.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+//Potencia
+
+
+double potencia(double x, int n){
+	
+	
+
+	if(n==0){
+		return 1;
+	}
+	else{
+		return x*potencia(x, n-1);
+	}
+}
+
+int main(){
+	double base;
+	int exponente;
+	cout<<"La base es :\n";
+	cin>> base;
+	cout<<"El exponente es :\n";
+	cin>> exponente;
+	cout<<"La potencia es :"<<potencia(base, exponente);
+	return 0;
+}
